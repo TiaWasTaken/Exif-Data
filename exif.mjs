5482d67fe22eb78143a081abfd7056c1ec8b06ea
Mattia Arganetto <151725770+TiaWasTaken@users.noreply.github.com>
2024-05-29 18:57:30 +0200

    Changed the output between txt file and terminal exif.mjs

--- exif.mjs
+++ exif.mjs
@@ -3,11 +3,10 @@ import path from "path";
 import { exiftool } from "exiftool-vendored";
 import chalk from "chalk";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 import readline from "readline";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,13 +19,13 @@ async function displayExif(imagePath) {
   try {
     const tags = await exiftool.read(imagePath);
 
-    let output = chalk.bold.underline(
-      `EXIF Data for ${chalk.green(imagePath)}:\n`,
-    );
+    let output = `EXIF Data for ${imagePath}:\n\n`;
+    let coloredOutput = chalk.bold.underline(`EXIF Data for ${chalk.green(imagePath)}:\n\n`);
 
     for (const [key, value] of Object.entries(tags)) {
       if (key !== "errors" && key !== "warnings") {
-        output += `${chalk.blue(key)}: ${value}\n`;
+        output += `${key}: ${value}\n`;
+        coloredOutput += `${chalk.blue(key)}: ${value}\n`;
       }
     }
 
@@ -35,23 +34,23 @@ async function displayExif(imagePath) {
       const lat = convertDMSToDD(tags.GPSLatitude, tags.GPSLatitudeRef);
       const lon = convertDMSToDD(tags.GPSLongitude, tags.GPSLongitudeRef);
       const googleMapsLink = createGoogleMapsUrl({ lat, lon });
-      output += `${chalk.blue("Google Maps Link")}: ${googleMapsLink}\n`;
+      output += `Google Maps Link: ${googleMapsLink}\n`;
+      coloredOutput += `${chalk.blue("Google Maps Link")}: ${googleMapsLink}\n`;
     } else {
-      output += `${chalk.blue("Google Maps Link")}: none\n`;
+      output += `Google Maps Link: none\n`;
+      coloredOutput += `${chalk.blue("Google Maps Link")}: none\n`;
     }
 
-    output += chalk.yellow("-----------\n");
+    output += "-----------\n";
+    coloredOutput += chalk.yellow("-----------\n");
 
     if (outputMode === "file") {
       fs.appendFileSync("exif_output.txt", output);
     } else {
-      console.log(output);
+      console.log(coloredOutput);
     }
   } catch (error) {
-    console.error(
-      chalk.red(`Error reading EXIF data from ${imagePath}:`),
-      error,
-    );
+    console.error(chalk.red(`Error reading EXIF data from ${imagePath}:`), error);
   }
 }
 
@@ -76,18 +75,16 @@ function createGoogleMapsUrl(coords) {
 function processDirectory(directoryPath) {
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
-      console.error(
-        chalk.red(`Error reading directory ${directoryPath}:`),
-        err,
-      );
+      console.error(chalk.red(`Error reading directory ${directoryPath}:`), err);
       return;
     }
 
     const imageFiles = files.filter(
       (file) =>
         fs.lstatSync(path.join(directoryPath, file)).isFile() &&
-        /\.(jpe?g|png|tiff)$/i.test(file),
+        /\.(jpe?g|png|tiff)$/i.test(file)
     );
+
     let processedCount = 0;
 
     imageFiles.forEach((file) => {
@@ -96,11 +93,7 @@ function processDirectory(directoryPath) {
         processedCount++;
         if (processedCount === imageFiles.length) {
           if (outputMode === "file") {
-            console.log(
-              chalk.green(
-                "\nAll images processed. Output written to exif_output.txt.",
-              ),
-            );
+            console.log(chalk.green("\nAll images processed. Output written to exif_output.txt."));
           } else {
             console.log(chalk.green("All images processed."));
           }
@@ -113,30 +106,23 @@ function processDirectory(directoryPath) {
 
 // Function to prompt user for output mode
 function promptUser() {
-  rl.question(
-    chalk.blue("\nSelect output mode: 1 for file, 2 for terminal: "),
-    (answer) => {
-      if (answer === "1") {
-        outputMode = "file";
-        if (fs.existsSync("exif_output.txt")) {
-          fs.unlinkSync("exif_output.txt"); // Clear the file if it already exists
-        }
-        processDirectory(imagesDirectory);
-        rl.close();
-      } else if (answer === "2") {
-        outputMode = "terminal";
-        processDirectory(imagesDirectory);
-        rl.close();
-      } else {
-        console.log(
-          chalk.red(
-            "\nInvalid selection. Please choose 1 for file or 2 for terminal.",
-          ),
-        );
-        promptUser();
+  rl.question(chalk.blue("\nSelect output mode: 1 for file, 2 for terminal: "), (answer) => {
+    if (answer === "1") {
+      outputMode = "file";
+      if (fs.existsSync("exif_output.txt")) {
+        fs.unlinkSync("exif_output.txt"); // Clear the file if it already exists
       }
-    },
-  );
+      processDirectory(imagesDirectory);
+      rl.close();
+    } else if (answer === "2") {
+      outputMode = "terminal";
+      processDirectory(imagesDirectory);
+      rl.close();
+    } else {
+      console.log(chalk.red("\nInvalid selection. Please choose 1 for file or 2 for terminal."));
+      promptUser();
+    }
+  });
 }
 
 // Directory containing the images
